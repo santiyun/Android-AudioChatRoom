428a859e2961e887076f26e60c4656ddd3abd723
Zane Wang <wodenhaowzg@163.com>
2019-01-24 12:41:46 +0800

    优化一些代码

--- app/src/main/java/com/ttt/chatroom/MainApplication.java
+++ app/src/main/java/com/ttt/chatroom/MainApplication.java
@@ -15,8 +15,7 @@ public class MainApplication extends Application {
         //1.设置SDK的回调接收类
         mMyTTTRtcEngineEventHandler = new MyTTTRtcEngineEventHandler(getApplicationContext());
         //2.创建SDK的实例对象
-//        TTTRtcEngine mTTTEngine = TTTRtcEngine.create(getApplicationContext(), <这里填申请到的appid>,
-        TTTRtcEngine mTTTEngine = TTTRtcEngine.create(getApplicationContext(), "test900572e02867fab8131651339518",
+        TTTRtcEngine mTTTEngine = TTTRtcEngine.create(getApplicationContext(), <这里填申请到的appid>,
                 mMyTTTRtcEngineEventHandler);
         if (mTTTEngine == null) {
             System.exit(0);
